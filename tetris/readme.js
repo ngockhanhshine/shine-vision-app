c3af8f8c15a6fe7beb6b45fcf9c749e65fb7083f
ngockhanhshine <ngockhanh.shine@gmail.com>
2026-03-29 17:23:11 +0700

    Add files via upload

---
+++ tetris/readme.js
@@ -0,0 +1,8 @@
+const readmeData = `HƯỚNG DẪN SỬ DỤNG
+1. Chỉnh màu sắc cho phù hợp với kính.
+2. Dùng phím mũi tên hoặc nút trên màn hình để di chuyển.
+3. Giữ phím Trái/Phải để di chuyển khối sang Trái/Phải
+4. Ấn phím Lên để xoay chiều của khối
+5. Giữ phím Xuống để di chuyển nhanh khối
+
+Chúc bạn chơi vui vẻ`;
