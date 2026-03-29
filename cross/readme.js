5eb73371990c1b764452f5fc05525f149166432e
ngockhanhshine <ngockhanh.shine@gmail.com>
2026-03-29 17:36:36 +0700

    Add files via upload

---
+++ cross/readme.js
@@ -0,0 +1,16 @@
+const readmeContent = `
+    <h1>HƯỚNG DẪN SỬ DỤNG</h1>
+    <p>Chào mừng bạn đến với Circle Cross Training...</p>
+    <h2>Phím tắt:</h2>
+    <ul>
+        <li><b>Ctrl + S / F12:</b> Mở bảng hướng dẫn này.</li>
+        <li><b>Esc:</b> Đóng bảng hướng dẫn.</li>
+    </ul>
+    <h2>Thao tác:</h2>
+    <ul>
+        <li><b>Vuốt Trái -> Phải:</b> Vào chế độ tập luyện.</li>
+        <li><b>Vuốt Phải -> Trái:</b> Về bảng cài đặt.</li>
+        <li><b>Khi đã hợp thị - Bấm vào màn hình để bắt đầu tính giờ.</li>
+        <li><b>Khi mất hợp thị - Bấm vào màn hình để kết thúc tính giờ.</li>
+    </ul>
+`;
