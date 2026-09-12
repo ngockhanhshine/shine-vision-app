6de6b9a64c437f26a0bf44324b2b83c88545016c
ngockhanhshine <ngockhanh.shine@gmail.com>
2026-09-12 20:28:47 +0700

    popup file

---
+++ popup-config.js
@@ -0,0 +1,11 @@
+// popup-config.js
+const POPUP_CONFIG = {
+    // 1. Đường dẫn ảnh Popup (đặt file popup.png chung folder hoặc đường dẫn link ảnh)
+    imageSrc: "popup.png",
+
+    // 2. Link chuyển hướng khi người dùng bấm vào ảnh (để trống "" nếu không muốn gắn link)
+    targetLink: "https://shinevisioncenter.com",
+
+    // 3. Mở link ở tab mới (true) hoặc tab hiện tại (false)
+    openInNewTab: true
+};
